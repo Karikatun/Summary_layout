778e80dd988539a3af4682ae39c556cad230eac5
Anton <shevchenko-anton2014@yandex.ru>
2019-02-21 22:09:21 +0300

    all blocks ready

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import './scss/styles.scss';
 import png from './images/PROFILE_ICON.png';
 import png1 from './images/Phone_Icon.png';
 import png2 from './images/SKILLS_ICON.png';
+import png3 from './images/EDUCATION_ICON.png';
+import png4 from './images/EXPERIENCE_ICON.png';
+import png5 from './images/SOFTWARE_ICON.png';
